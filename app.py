e1efe58961960cafa8d12558b143c3f8407ed125
Quoc Dinh <132872972+Fordinh4@users.noreply.github.com>
2024-01-06 22:51:55 -0700

    Working back and front end

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, request, jsonify, render_template, redirect
+from flask_pymongo import PyMongo
+from bson import ObjectId
 
-app = Flask(__name__)
-
+app = Flask(__name__, template_folder= "./templates", static_folder = './templates/static')
+app.config["MONGO_URI"] = "mongodb://localhost:27017/medical_database"
+mongo = PyMongo(app)
 
 
 # TODO: 1. create class object that can create the hospital and it's doctors (Hafiz)
@@ -9,10 +12,67 @@ app = Flask(__name__)
 
 # TODO: 2. create the function for algorithm sorting (Yash)
 
+#TODO: For Hafiz to implement
+def format_availability(available_days):
+    return {"Monday": [1, 2, 3], "Wednesday": [12, 13, 14, 15]}
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return render_template('homepage.html')
+
+@app.route("/patient_form")
+def patient_form():
+    return render_template('patientForm.html')  # Replace with your actual patient form template
+
+@app.route("/patients", methods=['POST'])
+def add_patient():
+    data = request.json  # Get the JSON data sent from the form
+    print(data)  # For debugging, print the data to the console
+    
+    # return a dict like this {"Monday": [1, 2, 3], "Wednesday": [12, 13, 14, 15]}
+    formatted_data = format_availability(data['availability'])
+
+    #TODO: availability
+    if request.method == 'POST':
+            # Prepare the document to insert
+        patient_document = {
+            "First_name": data['first_name'],
+            "Last_name": data['last_name'],
+            "Phone_number": data['phone_number'],
+            "Gender": data['gender'],
+            "Age": data['age'],          
+            "Availability": formatted_data,
+            "Symptoms": data['symptoms']
+        }
+
+        # Insert into the MongoDB collection
+        mongo.db.patients.insert_one(patient_document)
+
+    # Here, you would normally process the data and insert it into the database
+    print(list(mongo.db.patients.find()))
+    return jsonify({'msg': 'Patient added successfully'}) 
+
+@app.route("/admin_form")
+def login():
+    return render_template('adminForm.html')  # Replace with your actual login page template
+
+@app.route('/doctors', methods=['POST'])
+def add_doctor():
+    if request.method == 'POST':
+        data = request.json
+        formatted_data = format_availability(data)
+        # Example doctor document
+        doctor_document = {
+            "First_name": data['first_name'],
+            "Last_name": data['last_name'],
+            "Email": data['email'],
+            "Availability": formatted_data,
+            "Operations": data['operations']
+        }
+
+    # Insert into the MongoDB collection
+    mongo.db.doctors.insert_one(doctor_document)
+    return jsonify({'msg': 'Doctor added successfully'}) 
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ main.py
@@ -0,0 +1,12 @@
+# TODO: pull all the data down, use the authorize api from google calendar to send to the doctor to authorize, then add or delete task thing with it!
+# TODO: OTHER: log in page for admin
+
+
+
+
+
+def main():
+    print("hi")
+
+if __name__ == '__main__':
+    main()
